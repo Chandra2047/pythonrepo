2e3109b5ac2fa730722061ee05a92d3ec2619893
cpbhatt <bhattcp25@gmail.com>
2025-01-24 11:07:05 +0545

    first commit

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ app=Flask(__name__)
 @app.route('/')
 def webout():
 	return '<h1>DevOps is so much  very fun but its not easy!!</h1>'
-app.run(host='0.0.0.0',port=7000)
+app.run(host='0.0.0.0',port=80001:7000
+)
